6e777d41f647f9e10c6d4e787c1bbd6f3ff60f1b
diegoese <dsilvapiedra@gmail.com>
2023-07-17 19:51:03 -0300

    Agregar distorsion al sonido en smoke

--- js/smoketext.js
+++ js/smoketext.js
@@ -1,7 +1,46 @@
 const timeout_smoke = 21000;
 const timeout_vuelta = 21001;
 
-function startGlitchSoundEffect() { /*pausar audio, comenzar audio glitcheado a mano*/
+var song = document.getElementById("audioplayer");
+var distort = document.getElementById("audiodistort");
+
+function decreaseVolume(sound, intervalTime) {
+  var currentVolume = sound.volume;
+  var interval = setInterval(function () {
+    if (currentVolume > 0.1) {
+      currentVolume -= 0.05; // Disminuir el volumen en un 10% (puedes ajustar este valor según tus necesidades)
+      sound.volume = currentVolume;
+    } else {
+      clearInterval(interval); // Detener el intervalo cuando el volumen llega a 0
+      sound.pause();
+    }
+  }, intervalTime);
+}
+
+function increaseVolume(sound, intervalTime) {
+  var currentVolume = sound.volume;
+  var interval = setInterval(function () {
+    sound.play();
+    if (currentVolume < 1) {
+      currentVolume += 0.05; // Disminuir el volumen en un 10% (puedes ajustar este valor según tus necesidades)
+      sound.volume = currentVolume;
+    } else {
+      currentVolume = 1;
+      clearInterval(interval); // Detener el intervalo cuando el volumen llega a 0
+    }
+  }, intervalTime);
+}
+
+function startGlitchSoundEffect() {
+  decreaseVolume(song, 700);
+  distort.play();
+  setTimeout(() => {
+    decreaseVolume(distort, 1000); // Disminuir el volumen de snd1 gradualmente cada segundo (1000 milisegundos)
+  }, 27000);
+  setTimeout(() => {
+    increaseVolume(song, 700); // Disminuir el volumen de snd1 gradualmente cada segundo (1000 milisegundos)
+  }, 30000);
+  /*pausar audio, comenzar audio glitcheado a mano*/
 }
 function startSmokeEffect() {
   const smokeText = document.getElementById("smokeText");
@@ -16,11 +55,63 @@ function startSmokeEffect() {
   }, timeout_vuelta);
 }
 
+// function changeId() {
+//   const spanElement = document.getElementById("texto");
+//   spanElement.id = "smokeText";
+
+//   startSmokeEffect();
+// }
 function changeId() {
   const spanElement = document.getElementById("texto");
   spanElement.id = "smokeText";
 
   startSmokeEffect();
+
+  moveElementsRandomly();
+}
+
+function moveElementsRandomly() {
+  const elementsToMove = document.querySelectorAll(
+    ".links-navegacion, .button-container"
+  );
+  const originalPositions = []; // Almacena las posiciones originales de los elementos
+
+  // Guardar las posiciones originales
+  elementsToMove.forEach(function (element) {
+    originalPositions.push({
+      element: element,
+      left: element.offsetLeft,
+      top: element.offsetTop,
+    });
+  });
+
+  const interval = setInterval(function () {
+    elementsToMove.forEach(function (element) {
+      element.style.position = "absolute";
+      element.style.left = getRandomPosition() + "px";
+      element.style.top = getRandomPosition() + "px";
+    });
+  }, 1000); // Intervalo de 1 segundo (ajusta según tus necesidades)
+
+  setTimeout(function () {
+    clearInterval(interval); // Detener el intervalo después de 30 segundos
+
+    // Restaurar las posiciones originales
+    originalPositions.forEach(function (position) {
+      position.element.style.left = position.left + "px";
+      position.element.style.top = position.top + "px";
+    });
+  }, 30000); // 30 segundos (ajusta según tus necesidades)
+}
+
+function getRandomPosition() {
+  const maxWidth = window.innerWidth - 100; // Ajusta el ancho máximo de la posición aleatoria
+  const maxHeight = window.innerHeight - 100; // Ajusta la altura máxima de la posición aleatoria
+
+  const randomX = Math.floor(Math.random() * maxWidth);
+  const randomY = Math.floor(Math.random() * maxHeight);
+
+  return randomX;
 }
 
 let overlayTimeout;
@@ -47,8 +138,8 @@ function startLinternaEffect() {
     circle.style.opacity = "0";
     circleTimeout = setTimeout(function () {
       document.body.removeChild(circle);
-    }, 20000);
-  }, 20000);
+    }, 35000);
+  }, 35000);
 }
 
 // function startLinternaEffect() {
